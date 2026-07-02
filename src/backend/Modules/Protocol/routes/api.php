21b4a79d5804533b023d5da757e5aca1bf77f265
Dossantos <mercio@gmail.com>
2026-07-02 16:51:47 +0200

    fix: API USAGE

--- src/backend/Modules/Protocol/routes/api.php
+++ src/backend/Modules/Protocol/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Protocol\app\Http\Controllers\TopicController;
 
-Route::prefix('api')->middleware(['auth:sanctum'])->group(function () {
+Route::prefix('api/v1')->middleware(['auth:sanctum'])->group(function () {
     Route::post('topics', [TopicController::class, 'store'])->name('topic.store');
     Route::get('topics', [TopicController::class, 'index'])->name('topic.index');
     Route::apiResource('protocols', 'Modules\\Protocol\\app\\Http\\Controllers\\ProtocolController')->names('protocol');
