1d07712a6450139b8fdd5c68842b65cf512f3db9
GC <geniocossa4@gmail.com>
2026-07-09 01:57:53 +0200

    cros

---
+++ src/backend/config/cros.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => [
+        '*',
+    ],
+
+    'allowed_origins' => [
+        'iscisamonografias.vercel.app',
+    ],
+
+    'allowed_headers' => [
+        '*',
+    ],
+
+    'supports_credentials' => true,
+
+];
